b3cebe107ed1146e74d4e8578390ab7703d3e076
Omprakash  D <omugan07@gmail.com>
2024-04-20 22:44:06 +0530

    techstack section

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav_links");
 const links = document.querySelectorAll(".nav_links li");
 
+// check if an element is in the viewport or not
 const isInViewport = (element) => {
   const rect = element.getBoundingClientRect();
   return (
@@ -13,7 +14,7 @@ const isInViewport = (element) => {
   );
 };
 
-// HAMBURGER MENU
+// hamburger menu
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
   links.forEach((link) => {
@@ -22,6 +23,7 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("toggle");
 });
 
+// close the hamburger menu when a link is clicked
 links.forEach((link) => {
   link.addEventListener("click", () => {
     navLinks.classList.toggle("open");
@@ -32,7 +34,7 @@ links.forEach((link) => {
   });
 });
 
-// ACCORDION ANIMATION
+// accordion animation
 const acc = document.querySelectorAll(".accordion");
 acc.forEach((currentAcc) => {
   currentAcc.addEventListener("click", function () {
@@ -57,41 +59,6 @@ acc.forEach((currentAcc) => {
   });
 });
 
-const animateElement = (element, className) => {
-  if (isInViewport(element)) {
-    element.classList.add(className);
-  }
-};
-
-const animateAllElement = (elements, className, delay) => {
-  elements.forEach((el, index) => {
-    setTimeout(() => {
-      if (isInViewport(el)) {
-        el.classList.add(className);
-      }
-    }, (index + 1) * delay);
-  });
-};
-
-window.addEventListener("load", () => {
-  window.scrollTo(0, 0);
-});
-
-window.addEventListener("scroll", () => {
-  animateElement(
-    document.querySelector(".service_section__title h1"),
-    "animate"
-  );
-  animateAllElement(
-    document.querySelectorAll(".accordion_panel"),
-    "animate",
-    400
-  );
-  animateElement(document.querySelector(".works_section__title h1"), "animate");
-  animateAllElement(document.querySelectorAll(".project_card"), "animate", 400);
-  animateElement(document.querySelector(".show_more"), "animate");
-});
-
 const showMoreBtn = document.querySelector(".show_more");
 
 const sampleProjects = [
@@ -100,39 +67,62 @@ const sampleProjects = [
     description:
       "An enterprise resource planning (ERP) application for CEG Tech Forum(CTF) such as accounting, inventory, sales, and HR. Developed with Flutter for the frontend and Node.js for the backend. The app functions as a centralized platform for managing logistics items. ",
     technologies: "Flutter, Node.js, PostgreSQL",
+    type: "App",
   },
   {
     title: "Riddle of the Sphinx",
     description:
       "Riddle of the Sphinx is an interactive web-based game that challenges players to solve a series of puzzles and riddles. Our team designed and developed the game with a focus on engaging gameplay, captivating visuals, and immersive storytelling, resulting in an experience that transports players to a separate world.",
-    technologies: "React.js, Node.js, PostgreSQL, Express.js",
+    technologies: "React.js, Express.js, PostgreSQL",
+    type: "Website",
+    link: "https://ros.kurukshetraceg.org.in/",
   },
   {
     title: "Xceed & Karnival",
     description:
       "We developed a website for Xceed and Karnival, two annual cultural festivals organized by CEG Tech Forum(CTF). The website provides information about the events, such as the schedule, rules, and registration details. The website also allows participants to register for the events online.",
     technologies: "React.js",
+    type: "Website",
+    link: "https://xceed.kurukshetraceg.org.in/",
   },
   {
     title: "CEG Tech Forum",
     description:
       "We developed a website for CEG Tech Forum(CTF), a student-run organization at the College of Engineering, Guindy. The website provides information about the organization, such as its history, vision, and mission. This website showcases the various events organized by CTF, such as such as Kurukshetra and Vyuhaa. ",
     technologies: "React.js",
+    type: "Website",
+    link: "https://cegtechforum.in/",
   },
 ];
 
 // Function to generate HTML for a project card
 const generateProjectCard = (project) => {
   return `
-    <div class="project_card animate">
-      <div class="project_card__row1">
-        <h2>${project.title}</h2>
-        <p>${project.description}</p>
-      </div>
-      <div class="project_card__row2">
-        <span class="row2_circle"></span><span>${project.technologies}</span>
-      </div>
-    </div>
+    
+        <div class="project_card animate">
+          <div class="project_content">
+            <h3 class="project_subtitle">${project.type}</h3>
+            <h2 class="project_title">${project.title}</h2>
+
+            <p class="project_description">
+            ${project.description}
+            </p>
+          </div>
+
+          <div class="project_buttons">
+            <div class="project_techstack">
+              <i class="ri-stack-line"></i>${project.technologies}
+            </div>
+
+            <a
+              href=${project.link ? project.link : ""}
+              target="_blank"
+              class=${project.link ? "project_techstack" : "project_link__hide"}
+            >
+              <i class="ri-links-line"></i> View
+            </a>
+          </div>
+        </div>
   `;
 };
 
@@ -162,3 +152,43 @@ const loadMoreProjects = () => {
 };
 
 showMoreBtn.addEventListener("click", loadMoreProjects);
+
+// scroll animation
+const animateElement = (element, className) => {
+  if (isInViewport(element)) {
+    element.classList.add(className);
+  }
+};
+
+// scroll animation for multiple elements
+const animateAllElement = (elements, className, delay) => {
+  elements.forEach((el, index) => {
+    setTimeout(() => {
+      if (isInViewport(el)) {
+        el.classList.add(className);
+      }
+    }, (index + 1) * delay);
+  });
+};
+
+window.addEventListener("load", () => {
+  window.scrollTo(0, 0);
+});
+
+window.addEventListener("scroll", () => {
+  animateElement(document.querySelector("#service_title h1"), "animate");
+  animateAllElement(
+    document.querySelectorAll(".accordion_panel"),
+    "animate",
+    400
+  );
+  animateElement(document.querySelector("#works_title h1"), "animate");
+  animateElement(document.querySelector("#techstack_title h1"), "animate");
+  animateAllElement(document.querySelectorAll(".project_card"), "animate", 400);
+  animateAllElement(
+    document.querySelectorAll(".techstack_card"),
+    "animate",
+    400
+  );
+  animateElement(document.querySelector(".show_more"), "animate");
+});
